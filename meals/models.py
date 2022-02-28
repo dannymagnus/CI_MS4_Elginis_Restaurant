ea543875dc1b163c152ecae4eb7ddeb37d516f80
dannymagnus <mrdanrichards@gmail.com>
2022-02-28 21:16:35 +0000

    initial commit add Meals model

---
+++ meals/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Meals(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=3, decimal_places=1)
+    vegetarian = models.BooleanField(default=False)
+    vegan = models.BooleanField(default=False)
+    alergens = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='meals/')
