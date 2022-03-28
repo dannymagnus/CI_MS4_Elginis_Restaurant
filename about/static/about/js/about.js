40100652b0818487a134eafa8502b04c89516f01
dannymagnus <mrdanrichards@gmail.com>
2022-03-28 16:13:58 +0100

    attempt separate static js about

--- about/static/about/js/about.js
+++ about/static/about/js/about.js
@@ -2,7 +2,7 @@
  * To rotate images in the  reasons for us carousel
  * Credit:  https://getbootstrap.com/docs/5.0/components/carousel/ ]
  */
- var myCarousel = document.querySelector('#home-carousel');
+ var myCarousel = document.querySelector('#reasons-carousel');
  var carousel = new bootstrap.Carousel(myCarousel, {
      interval: 2000,
      wrap: true,
