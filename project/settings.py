8a6d9a44d6a41707e1398ff094cd0c1fd265e68b
dannymagnus <mrdanrichards@gmail.com>
2022-03-28 15:46:34 +0100

    set x frame options for amiresponsive

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'ALLOW-FROM http://ami.responsivedesign.is'
 
 ALLOWED_HOSTS = ['elginis-restaurant.herokuapp.com', 'localhost', '127.0.0.1', '*']
 
